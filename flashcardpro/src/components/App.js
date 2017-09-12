793ba77e171e97270f0e50f12b0e04e8e560d9ff
andy <andy@inniaccounts.co.uk>
2017-09-12 13:35:35 +0100

    stacks being shown

--- flashcardpro/src/components/App.js
+++ flashcardpro/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import StackList from './StackList';
+
 class App extends Component {
     render(){
         return <div>
                 <h2>Flashcard pro</h2>
+                <hr />
+                <StackList />
             </div>
     }
 }
